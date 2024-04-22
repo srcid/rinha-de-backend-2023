a35570ed9121bff3ce5267c197e75a6558892152
Alcides Ribeiro <alcidesribeirosampaioneto@gmail.com>
2024-04-21 21:09:47 -0300

    ✨ feat(schemes): added field aliases for match rinha de backend 2023 requirements

--- rinha_de_backend_2023/schemes.py
+++ rinha_de_backend_2023/schemes.py
@@ -6,13 +6,13 @@ from pydantic import BaseModel, ConfigDict, Field
 
 
 class NewPersonScheme(BaseModel):
-    nickname: str
-    name: str
-    birthDate: datetime
+    nickname: str = Field(alias="apelido")
+    name: str = Field(alias="nome")
+    birthDate: datetime = Field(alias="nascimento")
     stack: Optional[list[str]]
 
 
 class PersonScheme(NewPersonScheme):
     id: UUID
 
-    model_config = ConfigDict(from_attributes=True)
+    model_config = ConfigDict(from_attributes=True, populate_by_name=True)
